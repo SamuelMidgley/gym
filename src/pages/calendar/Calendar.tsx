521e90f27143a66dd32948d0eebf6b9590377682
Samuel Midgley <midgley.sam@gmail.com>
2022-12-31 02:29:14 +0000

    📅 initial calendar component work

--- src/pages/calendar/Calendar.tsx
+++ src/pages/calendar/Calendar.tsx
@@ -1,15 +1,119 @@
-import getDayArray from './utils'
+import React, { useCallback, useEffect, useState } from 'react'
+import { format } from 'date-fns'
+
+import getMonthArray, { IDay } from './utils'
 
 import './Calendar.scss'
+import DoubleLeftIcon from '../../components/icons/DoubleLeftIcon'
+import LeftIcon from '../../components/icons/LeftIcon'
+import RightIcon from '../../components/icons/RightIcon'
+import DoubleRightIcon from '../../components/icons/DoubleRight'
+
+interface ITableCell extends IDay {
+  onClickHandler: (e: React.MouseEvent<HTMLTableCellElement>) => void
+}
+
+function TableCell(props: ITableCell) {
+  const { date, thisMonth, onClickHandler } = props
+
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    <td onClick={onClickHandler}>
+      <div className={!thisMonth ? 'prev-month' : ''}>{date}</div>
+    </td>
+  )
+}
+
+interface IDateRow {
+  dayList: IDay[]
+  onClickHandler: (e: React.MouseEvent<HTMLTableCellElement>) => void
+}
+
+function DateRow(props: IDateRow) {
+  const { dayList, onClickHandler } = props
+
+  return (
+    <tr>
+      {dayList.map((day) => {
+        const { date, thisMonth } = day
+        return (
+          <TableCell
+            key={`${date}_${thisMonth}`}
+            date={date}
+            thisMonth={thisMonth}
+            onClickHandler={onClickHandler}
+          />
+        )
+      })}
+    </tr>
+  )
+}
 
 export default function Calendar() {
-  getDayArray()
+  const [month, setMonth] = useState(11)
+  const [year, setYear] = useState(2022)
+  const [monthArray, setMonthArray] = useState<IDay[][]>([])
+
+  useEffect(() => {
+    const array = getMonthArray(month, year)
+    setMonthArray(array)
+  }, [month, year])
+
+  getMonthArray(month, year)
+
+  const onClickHandler = useCallback(
+    (e: React.MouseEvent<HTMLTableCellElement>) => {
+      const target: HTMLElement = e.target as HTMLElement
+      let cellTarget: HTMLTableCellElement
+      if (target.nodeName === 'DIV') {
+        cellTarget = (e.target as HTMLTableCellElement)
+          .parentElement as HTMLTableCellElement
+      } else {
+        cellTarget = e.target as HTMLTableCellElement
+      }
+
+      const dayIndex = cellTarget.cellIndex
+      const weekIndex = (cellTarget.parentElement as HTMLTableRowElement)
+        .rowIndex
+
+      return [dayIndex, weekIndex]
+    },
+    []
+  )
+
   return (
     <>
       <header>
         <h1>Calendar</h1>
       </header>
       <main className="calendar-main">
+        <section className="calendar-main-header">
+          <button
+            type="button"
+            onClick={() => setYear((prevYear) => prevYear - 1)}
+          >
+            <DoubleLeftIcon />
+          </button>
+          <button
+            type="button"
+            onClick={() => setMonth((prevMonth) => prevMonth - 1)}
+          >
+            <LeftIcon />
+          </button>
+          <h2>{format(new Date(year, month, 1), 'LLLL yyyy')}</h2>
+          <button
+            type="button"
+            onClick={() => setMonth((prevMonth) => prevMonth + 1)}
+          >
+            <RightIcon />
+          </button>
+          <button
+            type="button"
+            onClick={() => setYear((prevYear) => prevYear + 1)}
+          >
+            <DoubleRightIcon />
+          </button>
+        </section>
         <table>
           <thead>
             <tr>
@@ -23,51 +127,66 @@ export default function Calendar() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
+            {monthArray.map((week, index) => (
+              <DateRow
+                key={index}
+                dayList={week}
+                onClickHandler={onClickHandler}
+              />
+            ))}
+            {/* <tr>
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
+              <TableCell value={1} onClickHandler={onClickHandler} />
             </tr>
             <tr>
               <td>2</td>
               <td>2</td>
-              <td>2</td>
-              <td>2</td>
-              <td>2</td>
-              <td>2</td>
+              <td className="highlighted h-start">
+                <div>2</div>
+              </td>
+              <td className="highlighted">
+                <div>2</div>
+              </td>
+              <td className="highlighted">
+                <div>2</div>
+              </td>
+              <td className="highlighted h-end">
+                <div>2</div>
+              </td>
               <td>2</td>
             </tr>
             <tr>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
+              {[3, 3, 3, 3, 3, 3, 3].map((num, index) => (
+                <TableCell
+                  value={num}
+                  key={index}
+                  onClickHandler={onClickHandler}
+                />
+              ))}
             </tr>
             <tr>
-              <td>4</td>
-              <td>4</td>
-              <td>4</td>
-              <td>4</td>
-              <td>4</td>
-              <td>4</td>
-              <td>4</td>
+              {[4, 4, 4, 4, 4, 4, 4].map((num, index) => (
+                <TableCell
+                  value={num}
+                  key={index}
+                  onClickHandler={onClickHandler}
+                />
+              ))}
             </tr>
             <tr>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-              <td>5</td>
-            </tr>
+              {[5, 5, 5, 5, 5, 5, 5].map((num, index) => (
+                <TableCell
+                  value={num}
+                  key={index}
+                  onClickHandler={onClickHandler}
+                />
+              ))}
+            </tr> */}
           </tbody>
         </table>
       </main>
--- src/pages/calendar/utils/index.ts
+++ src/pages/calendar/utils/index.ts
@@ -1,22 +1,67 @@
-import {
-  endOfMonth,
-  getDay,
-  startOfMonth,
-  getDate,
-  getYear,
-  getMonth,
-} from 'date-fns'
+import { endOfMonth, getDay, startOfMonth, getDate } from 'date-fns'
 
-export default function getDayArray() {
-  const year = 2022
-  const month = 12
-  const date = new Date(year, month - 1)
+export interface IDay {
+  date: number
+  thisMonth: boolean
+}
+
+function sliceIntoChunks(arr: IDay[], chunkSize: number) {
+  const res = []
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize)
+    res.push(chunk)
+  }
+  return res
+}
+
+export default function getMonthArray(month: number, year: number) {
+  console.time('getMonthArray')
+  const monthArray: IDay[] = []
+  const date = new Date(year, month)
 
   const start = startOfMonth(date)
-  const end = endOfMonth(date)
+  const startDay = getDay(start)
 
-  console.log(getDay(start))
-  console.log(getDate(start))
-  console.log(getDay(end))
-  console.log(getDate(end))
+  if (startDay !== 1) {
+    // Does not work for months starting with sunday (getDate returns 0)
+    // October 2023
+    // Month does not start on monday
+    const prevMonth = new Date(year, month - 1)
+    const prevMonthEnd = endOfMonth(prevMonth)
+    let prevMonthEndDate = getDate(prevMonthEnd)
+
+    for (let index = 0; index < startDay - 1; index += 1) {
+      const dayObject: IDay = { date: prevMonthEndDate, thisMonth: false }
+      monthArray.unshift(dayObject)
+      prevMonthEndDate -= 1
+    }
+  }
+
+  const end = endOfMonth(date)
+  const endDay = getDay(end)
+  const endDate = getDate(end)
+
+  for (let index = 1; index < endDate + 1; index += 1) {
+    const dayObject: IDay = { date: index, thisMonth: true }
+    monthArray.push(dayObject)
+  }
+
+  let remainingDays = 7 - endDay
+  const { length } = monthArray
+
+  if (length < 35) {
+    remainingDays += 7
+  }
+
+  if (remainingDays !== 0) {
+    for (let index = 1; index < remainingDays + 1; index += 1) {
+      const dayObject: IDay = { date: index, thisMonth: false }
+      monthArray.push(dayObject)
+    }
+  }
+
+  const processedMonthArray = sliceIntoChunks(monthArray, 7)
+
+  console.timeEnd('getMonthArray')
+  return processedMonthArray
 }
