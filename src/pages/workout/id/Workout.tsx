1445e949204deb441ff580be1caaab90d72968d2
Samuel Midgley <midgley.sam@gmail.com>
2023-01-15 21:50:18 +0000

    🏋️ First supabase call added

--- src/pages/workout/id/Workout.tsx
+++ src/pages/workout/id/Workout.tsx
@@ -1,8 +1,61 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import supabase from '../../../supabaseClient'
+
+interface IWorkout {
+  id: number
+  name: string
+  time: Date
+  location?: string
+}
 
 export default function Workout() {
+  const navigate = useNavigate()
   const { workoutId } = useParams()
 
-  console.log(workoutId)
+  if (workoutId === undefined) {
+    navigate('/gym/404')
+  }
+
+  const [loading, setLoading] = useState<boolean>(false)
+  const [workoutData, setWorkoutData] = useState<IWorkout>()
+
+  const getWorkout = async (id: number) => {
+    try {
+      setLoading(true)
+
+      const { data, error, status } = await supabase
+        .from('workout')
+        .select()
+        .eq('id', id)
+        .single()
+
+      if (data) {
+        setWorkoutData(data)
+      }
+
+      navigate('/gym/404')
+    } catch (error) {
+      navigate('/gym/404')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    console.log(workoutId)
+    if (workoutId === undefined) {
+      navigate('/gym/404')
+    }
+    const id = +workoutId
+    console.log(id)
+
+    if (!id) {
+      navigate('/gym/404')
+    }
+
+    getWorkout(id)
+  }, [workoutId, navigate])
+
   return <h1>{workoutId}</h1>
 }
